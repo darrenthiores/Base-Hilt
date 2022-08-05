493ad34c7a34219dd5bbb201a8485cd14809c367
icebeal <darrenthiores@gmail.com>
2022-08-05 22:24:07 +0700

    Initial commit

---
+++ app/src/main/java/com/dev/base/MyApplication.kt
@@ -0,0 +1,29 @@
+package com.dev.base
+
+import android.app.Application
+import androidx.paging.ExperimentalPagingApi
+import com.dev.base.di.useCaseModule
+import com.dev.base.di.viewModelModule
+import com.dev.core.di.databaseModule
+import com.dev.core.di.networkModule
+import com.dev.core.di.repositoryModule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.koin.android.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import timber.log.Timber
+
+open class MyApplication : Application() {
+
+    @ExperimentalCoroutinesApi
+    @ExperimentalPagingApi
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
