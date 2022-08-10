5157f3006c1d925a1a046089f725436cbd4664ab
icebeal <darrenthiores@gmail.com>
2022-08-10 09:23:10 +0700

    Fixing some annotation

--- core/src/main/java/com/dev/core/data/remote/source/RemoteDataSource.kt
+++ core/src/main/java/com/dev/core/data/remote/source/RemoteDataSource.kt
@@ -3,8 +3,9 @@ package com.dev.core.data.remote.source
 import com.dev.core.data.remote.service.ApiService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import javax.inject.Singleton
 
-@HiltViewModel
+@Singleton
 class RemoteDataSource @Inject constructor(
     private val apiService: ApiService
 ) {
